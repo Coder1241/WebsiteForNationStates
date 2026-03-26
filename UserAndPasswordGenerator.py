19d6e4484e56a7a90f56ba7803be39354033a15d
Coder1241 <adamsimpsonedward@gmail.com>
2026-03-25 20:41:28 -0400

    Adding counting storage to prevent user dupes

--- UserAndPasswordGenerator.py
+++ UserAndPasswordGenerator.py
@@ -13,8 +13,14 @@ def generate_random_alphanumeric(length, count):
     count += 1
     return random_string
 length = 10 #Set passwords to 10 digits/characters
-count = 0
-for i in range(2):
+with open('countstorage.txt', 'r') as f:
+    line = f.readline()
+    if line == '':
+        count = 0
+    else:
+        count = line
+times = int(input("How many accounts do you want to generate? "))
+for i in range(times):
     password = generate_random_alphanumeric(length, count)
     print(password)
     user = f'PASSWORD_FOR_{count}'
@@ -22,10 +28,12 @@ for i in range(2):
     dotenv.load_dotenv()
     username = os.getenv(user)
     #Checks for dupes
-    if username is not None:
+    while username is not None:
         count +=1
         user = f'PASSWORD_FOR_{count}'
     with open('users.txt', 'a') as userfile:
         userfile.write(user + '\n')
     with open('.env', 'a') as envfile:
         envfile.write(f"{user}='{password}'\n")
+with open('countstorage.txt', 'w') as e:
+    e.write(count)
