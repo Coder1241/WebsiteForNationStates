a7e8f298658d64e8ad1fc34a6aa167e2239adb17
Coder1241 <adamsimpsonedward@gmail.com>
2026-03-25 21:48:08 -0400

    Updated storing users and count value, mostly works until count value is stored, which it then begins to dupe.

--- UserAndPasswordGenerator.py
+++ UserAndPasswordGenerator.py
@@ -5,35 +5,28 @@ import dotenv
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-def generate_random_alphanumeric(length, count):
+def generate_random_alphanumeric(length):
     # Combine letters and digits from the string module constants
     characters = string.ascii_letters + string.digits
     # Use random.choices for Python 3.6+ (more efficient) or a generator expression
     random_string = ''.join(random.choices(characters, k=length))
-    count += 1
     return random_string
 length = 10 #Set passwords to 10 digits/characters
-with open('countstorage.txt', 'r') as f:
-    line = f.readline()
-    if line == '':
-        count = 0
-    else:
-        count = line
+count = 1
 times = int(input("How many accounts do you want to generate? "))
+dotenv.load_dotenv()
 for i in range(times):
-    password = generate_random_alphanumeric(length, count)
+    password = generate_random_alphanumeric(length)
     print(password)
     user = f'PASSWORD_FOR_{count}'
     env_path = dotenv.find_dotenv()
-    dotenv.load_dotenv()
     username = os.getenv(user)
     #Checks for dupes
     while username is not None:
         count +=1
         user = f'PASSWORD_FOR_{count}'
-    with open('users.txt', 'a') as userfile:
-        userfile.write(user + '\n')
+        username = os.getenv(user)
     with open('.env', 'a') as envfile:
         envfile.write(f"{user}='{password}'\n")
-with open('countstorage.txt', 'w') as e:
-    e.write(count)
+with open('users.txt', 'w') as e:
+        e.write(f'{count}')
